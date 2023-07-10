fa29b0db42cd6630250bd2e750301a78149ce195
sOfTheApes <SaulRS951@protonmail.com>
2023-07-10 10:09:47 -0700

    write todo for nix-shell

--- default.nix
+++ default.nix
@@ -2,11 +2,14 @@
 pkgs.mkShell {
 	packages = with pkgs; [ 
 		(python3.withPackages (ps: with ps; [
-			# Base (not in nixpkgs: open3d)
+			# Base
 			pydantic pygame numpy scipy opencv4 pillow prettytable matplotlib
 			# ROAR_Sim
 			imageio imageio-ffmpeg moviepy pandas pyrealsense2WithoutCuda pyserial
+			# TODO
+			# - open3d
+			# - https://drive.google.com/drive/folders/13JSejJED31cZHBbfIz_gyxxPmiqABOJj?usp=sharing
+			# - https://drive.google.com/drive/folders/1ejKIOp8_vXaTroA7prcCDrfQet9WL-oD?usp=sharing
 		]))
-		lazygit
 	];
 }
